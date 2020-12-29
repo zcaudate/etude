9952693acd9138d022347460c1759bf88418f4d4
Chris Zheng <z@caudate.me>
2020-12-29 17:16:27 +0000

    Cleaning up etude

--- Makefile
+++
@@ -1,5 +0,0 @@
-os-build:
-	cd infra/Docker/build && ./image_build.sh
-
-os-push:
-	cd infra/Docker/build && ./image_push.sh
--- infra/Ubuntu/build/inception.sh
+++ infra/Ubuntu/build/inception.sh
@@ -1,6 +1,6 @@
 cd /root/.emacs.d/infra/Ubuntu
+rm -R build
 mkdir build
-tangle.sh Ubuntu.org
-cd build
-chmod +x ./build.sh
-# ./build.sh
+tangle Main.org
+chmod +x build/build.sh
+cd build && ./build.sh
